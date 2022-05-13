69591195c44d0e7215516f06313be8dc06b9b47d
Pa Gutri <pagutri@gmail.com>
2022-05-13 10:53:02 -0500

    5. Añadir prueba para el Reader

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,32 @@
+const Reader = require("../../../app/lib/utils/Reader.js");
+
+describe("Tests for Reader", () => {
+    test("1. Read the file correctly", () => {
+        const explorers = Reader.readJsonFile("test/lib/utils/Reader_testfile.json");
+        expect(explorers).toStrictEqual([
+            {
+              "id": "6264d5d89f1df827eb84bb23",
+              "name": "Warren",
+              "email": "Todd@visualpartnership.xyz",
+              "credits": 508,
+              "enrollments": [
+                "Visual Thinking Intermedio",
+                "Visual Thinking Avanzado"
+              ],
+              "previousCourses": 1,
+              "haveCertification": true
+            },
+            {
+              "id": "6264d5d85cf81c496446b67f",
+              "name": "Lucinda",
+              "email": "Sexton@visualpartnership.xyz",
+              "credits": 677,
+              "enrollments": [
+                "Visual Thinking Avanzado"
+              ],
+              "previousCourses": 6,
+              "haveCertification": true
+            }
+        ]);
+    });
+});
