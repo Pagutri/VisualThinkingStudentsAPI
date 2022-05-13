fcc39a58af8b679f9e2444144b8bbbd13d267a7d
Pa Gutri <pagutri@gmail.com>
2022-05-13 13:34:56 -0500

    25. Crear método para devolver partners con más de 500 cŕeditos

--- app/lib/services/PartnersService.js
+++ app/lib/services/PartnersService.js
@@ -6,6 +6,10 @@ class PartnersService {
     static emailsOfCertifiedPartners(partners) {
         return partners.filter((partner) => partner.haveCertification == true).map((partner) => partner.email);
     }
+
+    static partnersAbove500(partners) {
+        return partners.filter((partner) => partner.credits > 500);
+    }
 }
 
 module.exports = PartnersService;
