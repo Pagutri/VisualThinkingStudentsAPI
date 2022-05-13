3a57fc36d253e6ebd7988476dcd71e78e11c63ba
Pa Gutri <pagutri@gmail.com>
2022-05-13 11:11:01 -0500

    7. Crear prueba para PartnersService

---
+++ test/lib/services/PartnersService.test.js
@@ -0,0 +1,34 @@
+const Reader = require("../../../app/lib/utils/Reader.js");
+const PartnersService = require("../../../app/lib/services/PartnersService.js");
+
+describe("Tests for PartnersService", () => {
+    test("1. Get the list of partners with all their properties", () => {
+        const partners = Reader.readJsonFile("test/lib/utils/Reader_testfile.json");
+        const listOfPartners = PartnersService.listOfPartners(partners);
+        expect(listOfPartners).toStrictEqual([
+            {
+              "id": "6264d5d89f1df827eb84bb23",
+              "name": "Warren",
+              "email": "Todd@visualpartnership.xyz",
+              "credits": 508,
+              "enrollments": [
+                "Visual Thinking Intermedio",
+                "Visual Thinking Avanzado"
+              ],
+              "previousCourses": 1,
+              "haveCertification": true
+            },
+            {
+              "id": "6264d5d85cf81c496446b67f",
+              "name": "Lucinda",
+              "email": "Sexton@visualpartnership.xyz",
+              "credits": 677,
+              "enrollments": [
+                "Visual Thinking Avanzado"
+              ],
+              "previousCourses": 6,
+              "haveCertification": true
+            }
+        ]);
+    });
+});
