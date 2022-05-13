3e2f5996ae00360f5953602aef712727c35d8299
Pa Gutri <pagutri@gmail.com>
2022-05-13 12:06:12 -0500

    17. Crear servidor

---
+++ app/lib/server.js
@@ -0,0 +1,13 @@
+const PartnersController = require("./controllers/PartnersController");
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+app.get("/", (request, response) => {
+    response.json({message: "Visual Thinking Students API welcome!"});
+});
+
+app.listen(port, () => {
+    console.log(`VisualThinkingStudents API in localhost:${port}`);
+});
