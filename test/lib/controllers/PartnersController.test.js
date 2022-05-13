4dc5ae339a0b4c354118c751801bfff9c6c311b4
Pa Gutri <pagutri@gmail.com>
2022-05-13 11:25:50 -0500

    9. Crear prueba para getPartners en PartnersController

---
+++ test/lib/controllers/PartnersController.test.js
@@ -0,0 +1,19 @@
+const PartnersController = require("../../../app/lib/controllers/PartnersController.js");
+
+describe("Tests for PartnersController", () => {
+    test("1. Test for getPartners", () => {
+        const partners = PartnersController.getPartners();
+        expect(partners[0]).toBe({
+              "id": "6264d5d89f1df827eb84bb23",
+              "name": "Warren",
+              "email": "Todd@visualpartnership.xyz",
+              "credits": 508,
+              "enrollments": [
+                "Visual Thinking Intermedio",
+                "Visual Thinking Avanzado"
+              ],
+              "previousCourses": 1,
+              "haveCertification": true
+            });
+    });
+});
