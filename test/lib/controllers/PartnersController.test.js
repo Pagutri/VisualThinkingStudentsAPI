c4a110b4bd5e47650cdbe63e07838020bae14879
Pa Gutri <pagutri@gmail.com>
2022-05-13 12:02:08 -0500

    15. Correr eslint

--- test/lib/controllers/PartnersController.test.js
+++ test/lib/controllers/PartnersController.test.js
@@ -4,16 +4,16 @@ describe("Tests for PartnersController", () => {
     test("1. Test for getPartners", () => {
         const partners = PartnersController.getPartners();
         expect(partners[0]).toStrictEqual({
-              "id": "6264d5d89f1df827eb84bb23",
-              "name": "Warren",
-              "email": "Todd@visualpartnership.xyz",
-              "credits": 508,
-              "enrollments": [
+            "id": "6264d5d89f1df827eb84bb23",
+            "name": "Warren",
+            "email": "Todd@visualpartnership.xyz",
+            "credits": 508,
+            "enrollments": [
                 "Visual Thinking Intermedio",
                 "Visual Thinking Avanzado"
-              ],
-              "previousCourses": 1,
-              "haveCertification": true
-            });
+            ],
+            "previousCourses": 1,
+            "haveCertification": true
+        });
     });
 });
