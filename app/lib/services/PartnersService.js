b8fe621fae6fbfd1b8a2914aad5f3ffc80f9d202
Pa Gutri <pagutri@gmail.com>
2022-05-13 13:14:33 -0500

    20. Crear método emailsOfCertifiedPartners

--- app/lib/services/PartnersService.js
+++ app/lib/services/PartnersService.js
@@ -2,6 +2,10 @@ class PartnersService {
     static listOfPartners(partners) {
         return partners;
     }
+
+    static emailsOfCertifiedPartners(partners) {
+        return partners.filter((partner) => partner.haveCertification == true).map((partner) => partner.email);
+    }
 }
 
 module.exports = PartnersService;
